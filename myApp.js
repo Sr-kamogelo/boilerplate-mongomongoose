1564a64e7547e710e9662ed17d945f7f4f044b4e
Mr Skywalker <kamogelooliphant140@gmail.com>
2022-07-27 16:23:04 +0200

    change

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
 let Person;
+const schema = mongoose.schema;
 const schemaOptions = {
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
 };
