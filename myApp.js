936cc7c5b7739b3d9690443bb66593de41d44f37
Mr Skywalker <kamogelooliphant140@gmail.com>
2022-07-28 16:55:36 +0200

    change

--- myApp.js
+++ myApp.js
@@ -88,14 +88,14 @@ const findEditThenSave = (personId, done) => {
   // .findById() method to find a person by _id with the parameter personId as search key.
   Person.findById(personId, function(err, person){
      if(err) return console.log(err);
-     
+
       // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
      person.favoriteFood.push(foodToAdd);
 
      // and inside the find callback - save() the updated Person.
     person.save((err, updatedPerson) => {
-      if(err) return console.log(err);
-     done(null,updatedPerson);
+      if (err) return console.log(err);
+     done(null, updatedPerson)
        });
     });
   };
